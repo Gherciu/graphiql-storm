179dd7ffa9c2b66d10144ed1e35a1f83d6ac690f
happy_hunter <gherciu553@gmail.com>
2019-02-08 18:32:38 +0200

    render graphiql-storm bug fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,8 @@ import {render} from 'react-dom'
 import App from './components/App.jsx'
 
 const renderGraphiQlStorm = (endpoints) => {
-    document.body.innerHTML = ''
     const root = document.createElement('div')
-    root.id = 'root'
+    root.id = 'root_graphiql-storm'
     document.body.appendChild(root)
     render(<App endpoints={endpoints}/>,root)
 }
